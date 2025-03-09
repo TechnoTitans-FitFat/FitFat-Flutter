865fe6b368b5147db6297faa8d24d00913725a54
hanahatem1 <hanahatemm88@gmail.com>
2025-03-10 00:11:59 +0200

    bottom bar elevation

--- lib/core/widgets/custom_bottom_nav_bar.dart
+++ lib/core/widgets/custom_bottom_nav_bar.dart
@@ -14,21 +14,41 @@ class CustomBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-      child: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          buildNavItem('imges/home.svg', 'Home', 0),
-          buildNavItem('imges/menu.svg', 'Menu', 1),
-          buildNavItem('imges/favourite.svg', 'Favourite', 2),
-          buildNavItem('imges/cart.svg', 'Cart', 3),
-          buildNavItem('imges/profile.svg', 'Profile', 4),
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 10,
+            spreadRadius: 2, 
+            offset: const Offset(0, -3), 
+          ),
         ],
-        currentIndex: selectedIndex,
-        selectedItemColor: AppLightColor.mainColor,
-        unselectedItemColor: Colors.grey,
-        onTap: onTapped,
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        child: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            buildNavItem('imges/home.svg', 'Home', 0),
+            buildNavItem('imges/menu.svg', 'Menu', 1),
+            buildNavItem('imges/favourite.svg', 'Favourite', 2),
+            buildNavItem('imges/cart.svg', 'Cart', 3),
+            buildNavItem('imges/profile.svg', 'Profile', 4),
+          ],
+          currentIndex: selectedIndex,
+          selectedItemColor: AppLightColor.mainColor,
+          unselectedItemColor: Colors.grey,
+          onTap: onTapped,
+          backgroundColor: Colors.transparent, 
+        ),
       ),
     );
   }
