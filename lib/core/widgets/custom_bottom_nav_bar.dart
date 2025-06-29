022a2530613fcd1309a7afa031fa6e705535bc58
hanahatem1 <hanahatemm88@gmail.com>
2025-06-30 00:00:46 +0300

    edit bottom bar done

--- lib/core/widgets/custom_bottom_nav_bar.dart
+++ lib/core/widgets/custom_bottom_nav_bar.dart
@@ -12,58 +12,71 @@ class CustomBottomNavBar extends StatelessWidget {
     required this.selectedIndex,
     required this.onTapped,
   });
+Widget _buildIcon(String iconPath, int index) {
+  return GestureDetector(
+    onTap: () => onTapped(index),
+    child: SvgPicture.asset(
+      iconPath,
+      height: 30,
+      color: selectedIndex == index
+          ? AppLightColor.mainColor
+          : AppLightColor.blackColor
+    ),
+  );
+}
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            blurRadius: 10,
-            spreadRadius: 2,
-            offset: const Offset(0, -3),
+    return Stack(
+    clipBehavior: Clip.none, 
+    children: [
+      Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 25),
+        decoration: BoxDecoration(
+          color: AppLightColor.whiteColor,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(25),
+            topRight: Radius.circular(25),
           ),
-        ],
-      ),
-      child: ClipRRect(
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
-        ),
-        child: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            buildNavItem(Assets.icons.home, 'Home', 0),
-            buildNavItem(Assets.icons.menu, 'Menu', 1),
-            buildNavItem(Assets.icons.favourite, 'Favourite', 2),
-            buildNavItem(Assets.icons.cart, 'Cart', 3),
-            buildNavItem(Assets.icons.profile, 'Profile', 4),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+              offset: const Offset(0, -3),
+            ),
           ],
-          currentIndex: selectedIndex,
-          selectedItemColor: AppLightColor.mainColor,
-          unselectedItemColor: Colors.grey,
-          onTap: onTapped,
-          backgroundColor: Colors.transparent,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildIcon(Assets.icons.home, 0),
+            _buildIcon(Assets.icons.menu, 1),
+            const SizedBox(width: 48), 
+            _buildIcon(Assets.icons.favourite, 3),
+            _buildIcon(Assets.icons.profile, 4),
+          ],
         ),
       ),
-    );
-  }
 
-  BottomNavigationBarItem buildNavItem(
-      String assetPath, String label, int index) {
-    return BottomNavigationBarItem(
-      icon: SvgPicture.asset(
-        assetPath,
-        color: selectedIndex == index
-            ? AppLightColor.mainColor
-            : AppLightColor.blackColor,
+      Positioned(
+        bottom: 19, 
+        left: MediaQuery.of(context).size.width / 2 - 28, 
+        child: GestureDetector(
+          onTap: () => onTapped(2),
+          child: Container(
+            padding: const EdgeInsets.all(14),
+            decoration:const BoxDecoration(
+              color: AppLightColor.mainColor,
+              shape: BoxShape.circle,
+             
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/chat.svg',
+            ),
+          ),
+        ),
       ),
-      label: label,
-    );
+    ],
+  );
   }
 }
