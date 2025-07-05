33759e8c5408b4b0f14db2b6a8c2328bfff1cc95
hanahatem1 <hanahatemm88@gmail.com>
2025-07-05 21:36:57 +0300

    fetch category sandwiches of menu

---
+++ lib/features/menu/sandwich/presentation/widgets/listview_sandwich.dart
@@ -0,0 +1,35 @@
+import 'package:fitfat/features/menu/sandwich/data/models/sandwich_model.dart';
+import 'package:fitfat/features/menu/sandwich/data/sandwich_cubit/sandwich_cubit.dart';
+import 'package:fitfat/features/menu/sandwich/presentation/widgets/list_view_item_sandwich.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ListviewSandwich extends StatelessWidget {
+  const ListviewSandwich({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SandwichCubit, SandwichState>(
+      builder: (context, state) {
+        if (state is SandwichLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is SandwichFailure) {
+          return Center(child: Text(state.errMessage));
+        } else if (state is SandwichSuccess) {
+          final List<SandwichModel> meals = state.data;
+          return ListView.builder(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            physics: const BouncingScrollPhysics(),
+            itemCount: meals.length,
+            itemBuilder: (context, index) {
+              return ListViewItemSandwich(meal: meals[index]);
+            },
+          );
+        } else {
+          return const SizedBox(); 
+        }
+      },
+    );
+  }
+}
